22b93e159f2cb32234bc4f33519add143b5b737a
shrestha <shresthachakrabortykgp@gmail.com>
2024-08-21 08:30:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import Editor from "./components/Editor";
+import ReactDOMServer from "react-dom/server";
+const App = () => {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `);
+    }, 250);
+    console.log(html);
+    console.log(css);
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+  return (
+    <>
+      <div className="flex min-w-[150px] p-4 bg-slate-300 pane top-pane  ">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onBeforeChange={setHtml}
+        />
+
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onBeforeChange={setCss}
+        />
+
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onBeforeChange={setJs}
+        />
+      </div>
+      {/* top pane done */}
+      <div className="  bg-yellow-300 flex h-[50vh] pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts allow-popups allow-same-origin allow-modals"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Editor.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+export default function Editor(props) {
+  const { language, displayName, value, onBeforeChange } = props;
+  const [open, setOpen] = useState(true);
+
+  //function handleChange() {}
+
+  return (
+    <div className="`editor-container ${open ? '' : 'colllapsed'}` p-2 grow basis-0 flex flex-col bg-blue-400">
+      <div className=" flex justify-between bg-black text-white py-4 px-6 rounded-l">
+        {displayName}
+        <button className="bg-white text-black px-3 py-1 rounded-lg">
+          o/c
+        </button>
+      </div>
+      <ControlledEditor
+        //onBeforeChange={onChange}
+        onBeforeChange={(_editor, _data, value) => {
+          onBeforeChange(value);
+          console.log(value);
+        }}
+        value={value}
+        //className="code-mirror-wrapper grow rounded-br-md rounded-bl-md flex-1"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+          theme: "material",
+        }}
+      />
+    </div>
+  );
+}
